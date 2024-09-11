c98097bb187c7e9915b27610dce0c78fb9583a20
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-09-11 19:09:16 +0900

    Title: 홀수만 출력 2, Time: 26ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240911/홀수만 출력 2/output-only-odd-2.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+print((input[1]...input[0]).filter { $0 % 2 == 1 }.map { String($0) }.reversed().joined(separator: " "))
