14e361aa60a7c6391b49934037a5b0269c87ed02
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-09-11 18:44:06 +0900

    Title: 살 수 있는 물건 2, Time: 10ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240911/살 수 있는 물건 2/things-able-to-buy-2.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+if n >= 3000 {
+    print("book")
+}
+else if n >= 1000 {
+    print("mask")
+}
+else if n >= 500 {
+    print("pen")
+}
+else {
+    print("no")
+}
