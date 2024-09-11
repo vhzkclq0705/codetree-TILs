4ea6089c2f57c41177f4f7a683e882008ff7252f
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-09-11 19:23:59 +0900

    Title: 자연수면 출력, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

--- 240911/자연수면 출력/output-only-natural-number.swift
+++ 240911/자연수면 출력/output-only-natural-number.swift
@@ -3,4 +3,4 @@ import Foundation
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (a, b) = (input[0], input[1])
 
-print(a >= 0 ? Array(repeating: String(a), count: b).joined() : 0)
+print(a > 0 ? Array(repeating: String(a), count: b).joined() : 0)
