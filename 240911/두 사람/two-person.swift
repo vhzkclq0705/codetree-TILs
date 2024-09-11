31d1ae6b97422dd0562a9e70bfb21cade9c94622
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-09-11 18:53:23 +0900

    Title: 두 사람, Time: 10ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240911/두 사람/two-person.swift
+++ 240911/두 사람/two-person.swift
@@ -3,4 +3,6 @@ import Foundation
 let inputA = readLine()!.split(separator: " ")
 let inputB = readLine()!.split(separator: " ")
 
-print((Int(inputA[0])! >= 19 && inputA[1] == "M") || (Int(inputB[0])! >= 19 && inputB[1] == "M") ? 1 : "")
+if (Int(inputA[0])! >= 19 && inputA[1] == "M") || (Int(inputB[0])! >= 19 && inputB[1] == "M") {
+    print(1)
+}
