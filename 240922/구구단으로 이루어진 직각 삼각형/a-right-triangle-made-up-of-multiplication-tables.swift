1f1cdd8b990b6f7831e6b6b7c68bd6bda35f4527
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-09-22 16:04:47 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

--- 240922/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.swift
+++ 240922/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.swift
@@ -3,5 +3,5 @@ import Foundation
 let n = Int(readLine()!)!
 
 for i in stride(from: n, to: 0, by: -1) {
-    print((1...i).map { "\(6 - i) * \($0) = \((6 - i) * $0)" }.joined(separator: " / "))
+    print((1...i).map { "\(n + 1 - i) * \($0) = \((n + 1 - i) * $0)" }.joined(separator: " / "))
 }
