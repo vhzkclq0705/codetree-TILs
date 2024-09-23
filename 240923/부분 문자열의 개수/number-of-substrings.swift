eb91070f089c9dd581a416821e3cd04d8749d6a0
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-09-23 15:43:06 +0900

    Title: 부분 문자열의 개수, Time: 13ms, Memory: 1MB, Status: Passed - CodeTree

--- 240923/부분 문자열의 개수/number-of-substrings.swift
+++ 240923/부분 문자열의 개수/number-of-substrings.swift
@@ -2,5 +2,16 @@ import Foundation
 
 let a = readLine()!
 let b = readLine()!
+var ans = 0
 
-print(a.components(separatedBy: b).count - 1)
+for i in 0...(a.count - b.count) {
+    let start = a.index(a.startIndex, offsetBy: i)
+    let end = a.index(start, offsetBy: b.count)
+    let range = start..<end
+
+    if String(a[range]) == b {
+        ans += 1
+    }
+}
+
+print(ans)
