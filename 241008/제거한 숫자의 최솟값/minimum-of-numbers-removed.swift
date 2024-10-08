724645eb0d12f594e9abbd67af63e98db936a62b
oh_jun <75382687+vhzkclq0705@users.noreply.github.com>
2024-10-08 21:48:07 +0900

    Title: 제거한 숫자의 최솟값, Time: 25ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 241008/제거한 숫자의 최솟값/minimum-of-numbers-removed.swift
+++ 241008/제거한 숫자의 최솟값/minimum-of-numbers-removed.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 let n = Int(readLine()!)!
-let nums = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-var m = 0
+let nums = readLine()!.split(separator: " ").map { Int64($0)! }.sorted()
+var m: Int64 = 0
 
 for i in 0..<n / 2 {
     m = max(m, nums[i] + nums[n - i - 1])
 }
-if n % 2 == 1 {
+
+if n / 2 == 1 {
     m = max(m, nums[n / 2])
 }
 
