2186df209595acdcf246bd54ced505c2394f404f
O_JUN <75382687+vhzkclq0705@users.noreply.github.com>
2024-12-11 16:43:23 +0900

    Title: 제거한 숫자의 최솟값, Time: 52ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 241211/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
+++ 241211/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 n = int(input())
 nums = sorted(map(int, input().split()))
-ans = sys.maxsize if not n % 2 else nums[n // 2]
+ans = 0 if not n % 2 else nums[n // 2]
 
 for i in range(n // 2):
-    ans = min(ans, nums[i] + nums[n - i - 1])
+    ans = max(ans, nums[i] + nums[n - i - 1])
 
 print(ans)
