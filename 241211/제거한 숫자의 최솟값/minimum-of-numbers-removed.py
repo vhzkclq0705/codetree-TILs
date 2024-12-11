32e40d0d7d72b3e9353843033c0518cdd4043f21
O_JUN <75382687+vhzkclq0705@users.noreply.github.com>
2024-12-11 16:45:53 +0900

    Title: 제거한 숫자의 최솟값, Time: 77ms, Memory: 19MB, Status: Runtime Error - CodeTree

--- 241211/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
+++ 241211/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 n = int(input())
 nums = sorted(map(int, input().split()))
-ans = 0 if not n % 2 else nums[n // 2]
+ans = 0 if not n % 2 else nums.pop()
 
 for i in range(n // 2):
     ans = max(ans, nums[i] + nums[n - i - 1])
